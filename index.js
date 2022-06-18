27f8658d150b4a8eeaf7bd30dfab7f98647721da
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-06-18 13:42:58 +0300

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+//sending data
+function submitData( name, email){
+return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( {
+      name,
+      email,
+    } )
+  } )
+  //response
+
+.then( function ( response ) {
+      return response.json()
+    } )
+    //accessing the new object
+.then( function ( object ) {
+document.body.innerHTML = object[ "id" ]
+    } )
+    //handling errors
+.catch( function ( error ) {
+document.body.innerHTML = error.message
+} )
+
+}
+
